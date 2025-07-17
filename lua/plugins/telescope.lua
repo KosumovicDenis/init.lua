3c1af7705b4051699bc066b1e77d14d187169459
KosumovicDenis <deniskosumi21@gmail.com>
2025-07-17 10:28:08 +0200

    feat: autoclose

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,19 +2,14 @@ return
     {
         "nvim-telescope/telescope.nvim",
 
-        tag = "0.1.5",
+        tag = "0.1.8",
 
         dependencies = {
             "nvim-lua/plenary.nvim"
         },
 
         config = function()
-            require('telescope').setup({
-                sync_install = false,
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
+            require('telescope').setup()
 
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
